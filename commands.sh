755b7adedaa54ec0f4adcabb2045f1cd08c100b2
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-10-12 13:38:39 +0000

    postgres

--- commands.sh
+++ commands.sh
@@ -1,13 +1,31 @@
-docker compose build
-docker compose up -d
-curl -L -o wwi.bak 'https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak'
-sudo docker cp wwi.bak sql1:/var/opt/mssql/backup
-sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/wwi.bak"' | tr -s ' ' | cut -d ' ' -f 1-2 
-sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd \
-   -S localhost -U SA -P Monika@1407! \
-   -Q 'RESTORE DATABASE WideWorldImporters FROM DISK = "/var/opt/mssql/backup/wwi.bak" WITH MOVE "WWI_P
-rimary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", MOVE "WWI_UserData" TO "/var/opt/mssql/data/Wi
-deWorldImporters_userdata.ndf", MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "W
-WI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"'
-sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'SELECT Name FROM sys.Databases'
-sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'SELECT TOP 10 StockItemID, StockItemName FROM WideWorldImporters.Warehouse.StockItems ORDER BY StockItemID'
+# docker compose build
+# docker compose up -d
+# curl -L -o wwi.bak 'https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak'
+# sudo docker cp wwi.bak sql1:/var/opt/mssql/backup
+# sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/wwi.bak"' | tr -s ' ' | cut -d ' ' -f 1-2 
+# sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd \
+#    -S localhost -U SA -P Monika@1407! \
+#    -Q 'RESTORE DATABASE WideWorldImporters FROM DISK = "/var/opt/mssql/backup/wwi.bak" WITH MOVE "WWI_P
+# rimary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", MOVE "WWI_UserData" TO "/var/opt/mssql/data/Wi
+# deWorldImporters_userdata.ndf", MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "W
+# WI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"'
+# sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'SELECT Name FROM sys.Databases'
+# sudo docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Monika@1407!' -Q 'SELECT TOP 10 StockItemID, StockItemName FROM WideWorldImporters.Warehouse.StockItems ORDER BY StockItemID'
+# sudo docker exec -u 0 -it sqlserver /bin/bash
+
+# apt-get update
+# apt-get install -y mssql-tools unixodbc-dev
+# ls /opt/mssql-tools/bin
+
+# docker exec -it mssql "bash"
+# /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Monika@1407!
+
+# CREATE DATABASE TestDB;
+# SELECT Name from sys.databases;
+# GO
+# USE TestDB;
+# CREATE TABLE Inventory (id INT, name NVARCHAR(50), quantity INT);
+# INSERT INTO Inventory VALUES (1, 'banana', 150); INSERT INTO Inventory VALUES (2, 'orange', 154);
+# GO
+# SELECT * FROM Inventory WHERE quantity > 152;
+# GO
